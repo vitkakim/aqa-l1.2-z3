ec2dccfba8cb820caaf190668d7b7b90ce635e7b
Victroria Anisimova <vitkakim@mail.ru>
2021-10-23 17:19:47 +0300

    inital commit

---
+++ src/test/java/ru/netology/rest/PostmaEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+    }
+}
