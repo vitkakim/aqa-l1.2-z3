64b3e63675961f6c44ba95ff308e619a45bb1618
Victroria Anisimova <vitkakim@mail.ru>
2021-10-23 17:25:35 +0300

    fixed test

--- src/test/java/ru/netology/rest/PostmaEchoTest.java
+++ src/test/java/ru/netology/rest/PostmaEchoTest.java
@@ -15,6 +15,6 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
     }
 }
